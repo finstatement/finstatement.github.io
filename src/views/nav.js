1b8968143845fbc80a4c1d18fc362588d699aa07
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-10 14:18:52 +0200

    Simple navigation added

---
+++ src/views/nav.js
@@ -0,0 +1,7 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/statements">Statements</a>
+    <a href="/create">Add Statement</a>
+    <a href="/login">Login</a>
+    <a href="/register">Register</a>
+</nav>
