81074ba81a1e054a86d15b23de3ab2209050e0f9
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-10 12:49:05 +0200

    Catalog template created

--- src/views/catalog.js
+++ src/views/catalog.js
@@ -1,3 +1,25 @@
 import { html } from "../lib/lit-html.js";
+import { repeat } from '../lib/directives/repeat.js'
 
 import * as statementService from "../data/statement.js";
+
+const catalogTemplate = (list) => html`
+<h1>Statements</h1>
+${list}
+`;
+
+const ListTemplate = (rooms) => html`
+<section>
+    ${repeat(rooms, r => r.objectId, roomCard)}
+</section>`;
+
+const roomCard = (room) => html`
+`;
+
+export async function catalogView(ctx) {
+    ctx.render(html`<p>Loading &hellip;</p>`);
+
+    const rooms = await roomService.getAll();
+
+    ctx.render(listTemplate(rooms));
+};
