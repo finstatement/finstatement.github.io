b959814866ebd05caef0906eb333b679b7392928
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-10 13:57:24 +0200

    Styles adapted to mobile first approach

--- src/views/catalog.js
+++ src/views/catalog.js
@@ -4,7 +4,7 @@ import { repeat } from '../lib/directives/repeat.js'
 import * as statementService from "../data/statement.js";
 
 const catalogTemplate = (list) => html`
-<h1>Statements</h1>
+<h1 class="heading">Statements</h1>
 ${list}
 `;
 
@@ -15,9 +15,9 @@ const listTemplate = (statements) => html`
 
 const statementCard = (statement) => html`
 <article class="statement-card">
-    <p>${statement.profession}</p>
-    <p>${statement.salary}</p>
-    <p>${statement.passive}</p>
+    <p>Job Title: ${statement.profession}</p>
+    <p>Salary: ${statement.salary}</p>
+    <p>Passive Income: ${statement.passive}</p>
     <p><a class="action" href="/statements/${statement.objectId}"><i class="fa-solid fa-up-right-from-square"></i></a></p>
 </article>`;
 
